f6b23322f6ea1844e4857444f3d25161e6652546
jteklak <jteklak@gmail.com>
2017-06-11 15:34:43 +0200

    change backgrCol

--- js/index.js
+++ js/index.js
@@ -1,42 +1,62 @@
 'use strict';
 /*JSON*/
 
-var jsonPracownicy = {
-    "pracownicy": [
-        {
-            "firstName": "Krystian",
-            "lastName": "Dziopa"
-    },
-        {
-            "firstName": "Anna",
-            "lastName": "Szapiel"
-    },
-        {
-            "firstName": "Piotr",
-            "lastName": "Żmuda"
-    }
-]
-};
-
-
-//var test = '{ "employees" : [' +
-//'{ "firstName":"John" , "lastName":"Doe" },' +
-//'{ "firstName":"Anna" , "lastName":"Smith" },' +
-//'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+//var jsonPracownicy = {
+//    "pracownicy": [
+//        {
+//            "firstName": "Krystian",
+//            "lastName": "Dziopa"
+//    },
+//        {
+//            "firstName": "Anna",
+//            "lastName": "Szapiel"
+//    },
+//        {
+//            "firstName": "Piotr",
+//            "lastName": "Żmuda"
+//    }
+//]
+//};
 //
-//console.log(test);
-
-console.log(jsonPracownicy);
-
-//var obj = JSON.parse(test);
 //
-//console.log(obj);
+////var test = '{ "employees" : [' +
+////'{ "firstName":"John" , "lastName":"Doe" },' +
+////'{ "firstName":"Anna" , "lastName":"Smith" },' +
+////'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+////
+////console.log(test);
+//
+//console.log(jsonPracownicy);
+//
+////var obj = JSON.parse(test);
+////
+////console.log(obj);
+//
+////for ( var i=0; i < imiona.length; i++ ) {
+////console.log( 'Imię numer ' + i + ': ' + imiona[ i ] );
+////}
+//
+//
+//jsonPracownicy.pracownicy.forEach(function (element, i) {
+//    console.log('Imię:' + element.firstName, 'Nazwisko:' + element.lastName, 'Index:', i);
+//});
+//
 
-//for ( var i=0; i < imiona.length; i++ ) {
-//console.log( 'Imię numer ' + i + ': ' + imiona[ i ] );
+
+//var red = document.getElementsByClassName('czerwony');
+//var green = document.getElementsByClassName('zielony');
+//var blue = document.getElementsByClassName('niebieski');
+//console.log(red);
+//console.log(green);
+//console.log(blue);
+
+
+//function changeColor() {
+//    document.getElementsByClassName('czerwony')[0].style.backgroundColor = "green";
 //}
 
+function changeColor() {
+    document.getElementsByClassName('czerwony')[0].className = "zielony";
+}
 
-jsonPracownicy.pracownicy.forEach( function( element, i ) {
-console.log('Imię:'+element.firstName, 'Nazwisko:'+ element.lastName,'Index:', i );
-} );
+document.getElementsByClassName('niebieski')[0].onclick = changeColor;
